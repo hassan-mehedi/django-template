3e6551a87587bfa4732858c29a06cd05b5437edd
hassan-mehedi <howlader.mehedihassan@gmail.com>
2024-10-22 23:32:40 +0600

    chore: implement custom user and user admin

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,80 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
-# Register User model
-admin.site.register(User)
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("email",)  # Add other fields you want to show in creation form
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = ("email",)  # Add other fields you want to show in change form
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ("email", "age", "gender", "is_staff", "is_active", "is_superuser")
+    list_filter = ("is_staff", "is_active", "is_superuser", "gender")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("age", "gender")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "age",
+                    "gender",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+    search_fields = ("email",)
+    ordering = ("email",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
+
+# Unregister the default UserAdmin if it was previously registered
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
+
+# Register your custom UserAdmin
+admin.site.register(User, CustomUserAdmin)
--- user/models.py
+++ user/models.py
@@ -1,37 +1,64 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 
 from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    # Choices
-    genders = (
-        ("Male", "Male"),
-        ("Female", "Female"),
-        ("Others", "Others"),
-        ("N/A", "Not Specified"),
+    MALE = "Male"
+    FEMALE = "Female"
+    OTHERS = "Others"
+    NOT_SPECIFIED = "N/A"
+
+    GENDER_CHOICES = [
+        (MALE, "Male"),
+        (FEMALE, "Female"),
+        (OTHERS, "Others"),
+        (NOT_SPECIFIED, "Not Specified"),
+    ]
+
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(_("first name"), max_length=50, blank=True)
+    last_name = models.CharField(_("last name"), max_length=50, blank=True)
+    email = models.EmailField(_("email address"), max_length=50, unique=True)
+    age = models.PositiveIntegerField(blank=True, default=0)
+    gender = models.CharField(
+        max_length=10, choices=GENDER_CHOICES, default=NOT_SPECIFIED, blank=True
+    )
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
     )
 
-    # Fields
-    user_id = models.AutoField(primary_key=True)
-    email = models.EmailField(max_length=50, unique=True)
-    age = models.PositiveIntegerField(blank=True, default=0)
-    gender = models.CharField(max_length=10, choices=genders, blank=True, default="N/A")
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
     objects = UserManager()
 
     class Meta:
         db_table = "users"
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
         constraints = [
             models.UniqueConstraint(fields=["email"], name="unique_email"),
             models.UniqueConstraint(fields=["user_id"], name="unique_user_id"),
-            models.CheckConstraint(
-                check=models.Q(gender__in=["Male", "Female", "Others", "N/A"]),
-                name="gender_check",
-            ),
         ]
 
     def __str__(self):
-        return self.username
+        return self.email
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.first_name
